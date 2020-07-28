a171d5fafaf2a9976d58f8e5695114303c07d4b7
Jan Redmann <janredmann1@gmail.com>
2020-07-28 14:28:14 +0200

    build

---
+++ client/build/precache-manifest.28f86d6ff87661de8d19b9388fe8a52d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aae699a86677d2434a938b13544c45a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41f6606676d5e06e2b0e",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d615bd64ac0ccb7bbdca",
+    "url": "/static/css/main.4d21caba.chunk.css"
+  },
+  {
+    "revision": "41f6606676d5e06e2b0e",
+    "url": "/static/js/2.491d4e66.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.491d4e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d615bd64ac0ccb7bbdca",
+    "url": "/static/js/main.f98d49e1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
